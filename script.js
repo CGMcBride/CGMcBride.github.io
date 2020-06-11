7e05da0597305b0ee6a709bc7476ab512b82f4d9
CGMcBride <connermcbride@ymail.com>
2020-06-10 19:27:22 -0500

    added js, pics and created links

---
+++ script.js
@@ -0,0 +1,26 @@
+// var card = document.getElementById("cardDiv")
+
+// function setCard() {
+//     for (let i = 0; i < 8; i++) {
+//         console.log('works');
+
+//         var oneCard = `<div class="col-sm-6 col-md-4 col-lg-3 mb-5">
+//     <div class="card style" style="width: 100%;">
+//         <img src="assets/images/portfolio/Project 1 image.jpg" class="card-img-top" alt="My name"
+//             height="235px" />
+//         <div class="card-body">
+//             <h5 class="card-title">Portifolio</h5>
+//             <p class="card-text">
+//                 Some quick example text to build on the card title and make up
+//                 the bulk of the card's content.
+//             </p>
+//             <a href="#" target="_blank" class="btn btn-danger">GitHub</a>
+//             <a href="https://CGMcBride.github.io" target="_blank" class="btn btn-info">Website</a>
+//         </div>
+//     </div>
+// </div>`
+//         card.innerHTML = oneCard;
+//     }
+// }
+
+// setCard()
